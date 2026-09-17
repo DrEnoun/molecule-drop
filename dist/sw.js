2b2ae7da3ef10b01fc7a06e1bbc118de158d67c4
DR ENOUN <nurain2942@uitm.edu.my>
2026-09-17 23:30:28 +0800

    Update dist/sw.js

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='molecule-drop-v4';
+const CACHE='molecule-drop-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./dr-nam-mascot.webp'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
